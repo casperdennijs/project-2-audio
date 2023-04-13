dcce9644da7f41d829280f81eeeea9cbc9990fb5
Casper <casper.dennijs@outlook.com>
2023-04-13 14:05:44 +0200

    Islands interaction

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,13 @@
 let islandCounter = -1;
 const islands = document.querySelectorAll(".island");
 
-setInterval(loop, 1000);
+let island0status = 1;
+let island1status = 1;
+let island2status = 1;
+let island3status = 1;
+let island4status = 1;
+
+setInterval(loop, 500);
 
 function loop() {
     if (islandCounter < 4) {
@@ -20,29 +26,84 @@ function sound() {
     islands.forEach(element => element.classList.remove("animation"))
     switch(islandCounter) {
         case 0:
-            synth.triggerAttack("D4", now);
-            synth.triggerRelease("D4", now + 1);
-            islands[islandCounter].classList.add("animation");
+            if (island0status == 1) {
+                synth.triggerAttack("D4", now);
+                synth.triggerRelease("D4", now + 1);
+                islands[islandCounter].classList.add("animation");
+            }
             break;
         case 1:
-            synth.triggerAttack("F4", now);
-            synth.triggerRelease("F4", now + 1);
-            islands[islandCounter].classList.add("animation");
+            if (island1status == 1) {
+                synth.triggerAttack("F4", now);
+                synth.triggerRelease("F4", now + 1);
+                islands[islandCounter].classList.add("animation");
+            }
             break;
         case 2:
-            synth.triggerAttack("A4", now);
-            synth.triggerRelease("A4", now + 1);
-            islands[islandCounter].classList.add("animation");
+            if (island2status == 1) {
+                synth.triggerAttack("A4", now);
+                synth.triggerRelease("A4", now + 1);
+                islands[islandCounter].classList.add("animation");
+            }
             break;
         case 3:
-            synth.triggerAttack("C5", now);
-            synth.triggerRelease("C5", now + 1);
-            islands[islandCounter].classList.add("animation");
+            if (island3status == 1) {
+                synth.triggerAttack("C5", now);
+                synth.triggerRelease("C5", now + 1);
+                islands[islandCounter].classList.add("animation");
+            }
             break;
         case 4:
-            synth.triggerAttack("E5", now);
-            synth.triggerRelease("E5", now + 1);
-            islands[islandCounter].classList.add("animation");
+            if (island4status == 1) {
+                synth.triggerAttack("E5", now);
+                synth.triggerRelease("E5", now + 1);
+                islands[islandCounter].classList.add("animation");
+            }
             break;
     }
 }
+
+islands[0].addEventListener("click", () => {
+    if (island0status == 0) {
+        island0status = 1
+    } else {
+        island0status = 0
+    }
+    islands[0].classList.toggle("hidden")
+})
+
+islands[1].addEventListener("click", () => {
+    if (island1status == 0) {
+        island1status = 1
+    } else {
+        island1status = 0
+    }
+    islands[1].classList.toggle("hidden")
+})
+
+islands[2].addEventListener("click", () => {
+    if (island2status == 0) {
+        island2status = 1
+    } else {
+        island2status = 0
+    }
+    islands[2].classList.toggle("hidden")
+})
+
+islands[3].addEventListener("click", () => {
+    if (island3status == 0) {
+        island3status = 1
+    } else {
+        island3status = 0
+    }
+    islands[3].classList.toggle("hidden")
+})
+
+islands[4].addEventListener("click", () => {
+    if (island4status == 0) {
+        island4status = 1
+    } else {
+        island4status = 0
+    }
+    islands[4].classList.toggle("hidden")
+})
